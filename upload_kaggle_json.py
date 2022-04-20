1415c2addae9f8e005000e9294ffcef127699f6b
SihanA <57210715+Sihan-A@users.noreply.github.com>
2022-04-20 20:18:41 +0900

    Update upload_kaggle_json.py

--- upload_kaggle_json.py
+++ upload_kaggle_json.py
@@ -1,9 +1,11 @@
 def upload_kaggle_json():
-    """ Upload kaggle.json to colab environment
+    """
+    Upload kaggle.json to colab environment
     """
     from google.colab import files
+    import os
+    os.chdir("/content")
     files.upload()
-    !mkdir -p ~/.kaggle
-    !cp kaggle.json ~/.kaggle/
-    !ls ~/.kaggle
-    !chmod 600 /root/.kaggle/kaggle.json
+    os.mkdir("/.kaggle")
+    os.rename("/content/kaggle.json", "/.kaggle/kaggle.json")
+    os.chmod("/.kaggle/kaggle.json", 600)
