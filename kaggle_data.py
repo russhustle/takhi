ae70c18c38688daa4dca2dfc1c02f0a9b702ce17
SihanA <57210715+Sihan-A@users.noreply.github.com>
2022-04-20 17:16:53 +0900

    Update kaggle_data.py

--- kaggle_data.py
+++ kaggle_data.py
@@ -1,21 +1,10 @@
-from google.colab import files
-uploaded = files.upload() # choose kaggle.json
-!mv /content/kaggle.json /root/.kaggle
-# !chmod 600 /root/.kaggle/kaggle.json
 
-# kaggle API
-!kaggle datasets download -d ajayrana/hymenoptera-data
-# then unzip
-
-
-## version 2
-#!pip install -q kaggle
-from google.colab import files
-files.upload()
-!mkdir -p ~/.kaggle
-!cp kaggle.json ~/.kaggle/
-!ls ~/.kaggle
-!chmod 600 /root/.kaggle/kaggle.json
-
-!kaggle datasets download -d tongpython/cat-and-dog
-!unzip cat-and-dog.zip
+def upload_kaggle_json():
+    """ Upload kaggle.json to colab environment
+    """
+    from google.colab import files
+    files.upload()
+    !mkdir -p ~/.kaggle
+    !cp kaggle.json ~/.kaggle/
+    !ls ~/.kaggle
+    !chmod 600 /root/.kaggle/kaggle.json
