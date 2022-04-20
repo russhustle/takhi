b230b06e8f399b343bc5f6d406178089b12d5025
SihanA <57210715+Sihan-A@users.noreply.github.com>
2022-04-20 20:27:56 +0900

    Update upload_kaggle_json.py

--- upload_kaggle_json.py
+++ upload_kaggle_json.py
@@ -6,6 +6,6 @@ def upload_kaggle_json():
     import os
     os.chdir("/content")
     files.upload()
-    os.mkdir("/.kaggle")
+    os.mkdir("/root/.kaggle")
     os.rename("/content/kaggle.json", "/root/.kaggle/kaggle.json")
     os.chmod("/root/.kaggle/kaggle.json", 600)
