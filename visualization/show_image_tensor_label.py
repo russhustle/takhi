271f08f68ae268949469af71ee39dc3196f5f436
SihanA <57210715+Sihan-A@users.noreply.github.com>
2022-04-25 09:43:47 +0900

    update

--- visualization/show_image_tensor_label.py
+++ visualization/show_image_tensor_label.py
@@ -1,10 +1,28 @@
 import numpy as np
+import torch
 import matplotlib.pyplot as plt
 from visualization.inverse_normalize import inverse_normalize
 
 def show_image_tensor_label(image_tensor, label, label_classes=None, mean=None, std=None):
+    """_summary_
+
+    Args:
+        image_tensor (_type_): _description_
+        label (_type_): _description_
+        label_classes (_type_, optional): _description_. Defaults to None.
+        mean (_type_, optional): _description_. Defaults to None.
+        std (_type_, optional): _description_. Defaults to None.
+    """
+    
     if mean is not None:
-        images_grid = inverse_normalize(image_tensor, mean, std) 
+        mean = torch.as_tensor(mean, dtype=image_tensor.dtype, device=image_tensor.device)
+        std = torch.as_tensor(std, dtype=image_tensor.dtype, device=image_tensor.device)
+        if mean.ndim == 1:
+            mean = mean.view(-1, 1, 1)
+        if std.ndim == 1:
+            std = std.view(-1, 1, 1)
+        image_tensor.mul_(std).add_(mean)
+    
     image_numpy = image_tensor.numpy()
     image_numpy_transposed = np.transpose(a=image_numpy, axes=(1,2,0))
     plt.title(f"Label: {label}" if label_classes is None else label_classes[label])
