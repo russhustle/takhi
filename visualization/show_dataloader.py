142533f598f6990f1b15ec5b185cfc275a82ee16
SihanA <57210715+Sihan-A@users.noreply.github.com>
2022-04-25 09:38:32 +0900

    update

--- visualization/show_dataloader.py
+++ visualization/show_dataloader.py
@@ -1,11 +1,10 @@
+import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from torchvision.utils import make_grid
-from visualization.inverse_normalize import inverse_normalize
 
 def show_dataloader(dataloader, mean=None, std=None, num_imgs=4, label_classes=None):
     """ Show sample images in a PyTorch dataloader.
-
     Args:
         dataloader (Dataloader): PyTorch dataloader to show.
         mean (List, optional): Mean value for each channel. Defaults to None.
@@ -19,12 +18,20 @@ def show_dataloader(dataloader, mean=None, std=None, num_imgs=4, label_classes=N
     if label_classes is not None:
         labels = [label_classes[i] for i in labels]
     images_grid = make_grid(images[:num_imgs])
+    
+    # Un-normalize
     if mean is not None:
-        images_grid = inverse_normalize(image_tensor=images_grid, mean=mean, std=std)
+        mean = torch.as_tensor(mean, dtype=images_grid.dtype, device=images_grid.device)
+        std = torch.as_tensor(std, dtype=images_grid.dtype, device=images_grid.device)
+        if mean.ndim == 1:
+            mean = mean.view(-1, 1, 1)
+        if std.ndim == 1:
+            std = std.view(-1, 1, 1)
+        images_grid.mul_(std).add_(mean)
+    
     images_numpy = images_grid.numpy()
     images_transposed_numpy = np.transpose(images_numpy, axes=(1,2,0))
     plt.figure(figsize=(num_imgs*5, num_imgs*3))
     plt.axis("off")
     plt.title(f"Label: {labels}")
     plt.imshow(images_transposed_numpy)
-    
