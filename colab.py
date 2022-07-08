7543c46d176478a8836da1648d411a5993d43edc
SihanA <57210715+Sihan-A@users.noreply.github.com>
2022-07-08 11:06:46 +0900

    update

--- colab.py
+++ colab.py
@@ -4,10 +4,7 @@ drive.mount('/content/drive')
 
 # 2. Create a soft link of direcctory in content
 import os
-CHAPTER_PATH = "/content/drive/MyDrive/02-udemy/udemy-ds-ml-bootcamp/17-K-Means-Clustering"
-CONTENT_PATH = "/content/KMEANS"
-os.symlink(src=CHAPTER_PATH, dst=CONTENT_PATH, target_is_directory=True)
-os.chdir("/content")
-
-
-
+DIR_PATH = "/content/drive/MyDrive/02-udemy/udemy-ds-ml-bootcamp/17-K-Means-Clustering"
+DST_PATH = "/content/KMEANS"
+os.symlink(src=DIR_PATH, dst=DST_PATH, target_is_directory=True)
+os.chdir(DST_PATH)
