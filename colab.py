d11da42e4134c7bbae1506e468514f1be3e9b1df
SihanA <57210715+Sihan-A@users.noreply.github.com>
2022-07-19 22:31:02 +0900

    kaggle.json

--- colab.py
+++ colab.py
@@ -13,3 +13,23 @@ os.chdir('/content')
 from IPython.display import Image
 PCA_PATH = os.path.join(DST_PATH, 'images/PCA.png')
 Image(PCA_PATH)
+
+# 4. Upload kaggle.json to colab
+
+from google.colab import files
+import os
+
+def upload_kaggle_json():
+    """ Upload kaggle.json to colab environment
+    Usage:
+        !git clone https://github.com/Sihan-A/sihan_utils.git
+        from sihan_utils.colab import upload_kaggle_json
+        upload_kaggle_json()
+        !kaggle datasets download -d berkeleyearth/climate-change-earth-surface-temperature-data
+    """
+    os.chdir("/content")
+    files.upload()
+    os.mkdir("/root/.kaggle")
+    os.rename("/content/kaggle.json", "/root/.kaggle/kaggle.json")
+    os.chmod("/root/.kaggle/kaggle.json", 600)
+
