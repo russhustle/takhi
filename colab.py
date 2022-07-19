f52d3f8abb5541eab9cc8519cdb0145115a302f0
SihanA <57210715+Sihan-A@users.noreply.github.com>
2022-07-19 13:12:30 +0900

    Update colab.py

--- colab.py
+++ colab.py
@@ -7,4 +7,9 @@ import os
 DIR_PATH = "/content/drive/MyDrive/02-udemy/udemy-ds-ml-bootcamp/17-K-Means-Clustering"
 DST_PATH = "/content/KMEANS"
 os.symlink(src=DIR_PATH, dst=DST_PATH, target_is_directory=True)
-os.chdir(DST_PATH)
+os.chdir('/content')
+
+# 3. Show an image in colab
+from IPython.display import Image
+PCA_PATH = os.path.join(DST_PATH, 'images/PCA.png')
+Image(PCA_PATH)
