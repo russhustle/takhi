5e9d82b5a1e2fcff4fe9d69df2be8296e49a0ab6
SihanA <57210715+Sihan-A@users.noreply.github.com>
2022-07-21 14:04:05 +0900

    update

--- dataset/CIFAR10.py
+++ dataset/CIFAR10.py
@@ -5,6 +5,10 @@ import torch
 from pytorch_lightning import LightningDataModule
 import os
 
+cifar10_mean=[0.49139968, 0.48215841, 0.44653091]
+cifar10_std =[0.24703223, 0.24348513, 0.26158784]
+num_workers = int(os.cpu_count()/2)
+
 def cifar10_dataloaders(BATCH_SIZE=32):
     """ Load CIFAR10 dataset as train, validation and test dataloaders.
     Args:
@@ -14,11 +18,14 @@ def cifar10_dataloaders(BATCH_SIZE=32):
         val_dataloader
         test_dataloader
     """
-    # Transoform
-    mean=[0.49139968, 0.48215841, 0.44653091]
-    std =[0.24703223, 0.24348513, 0.26158784]
-    train_transform = Compose([RandomCrop(32, padding=4), RandomHorizontalFlip(), ToTensor(), Normalize(mean=mean, std=std),])
-    val_test_transform = Compose([ToTensor(), Normalize(mean=mean, std=std),])
+    train_transform = Compose([
+        RandomCrop(32, padding=4), RandomHorizontalFlip(), ToTensor(),
+        Normalize(mean=cifar10_mean, std=cifar10_std),
+        ])
+    val_test_transform = Compose([
+        ToTensor(), Normalize(mean=cifar10_mean, std=cifar10_std),
+        ])
+    
     # Datasets
     train_dataset = CIFAR10(root=os.getcwd(), train=True, download=True, transform=train_transform)
     val_length = 5000
@@ -29,11 +36,18 @@ def cifar10_dataloaders(BATCH_SIZE=32):
     val_dataset = CIFAR10(root=os.getcwd(), train=True, download=True, transform=val_test_transform)
     _, val_dataset = random_split(dataset=val_dataset, lengths=lengths, generator=generator)
     test_dataset = CIFAR10(root=os.getcwd(), train=False, download=False, transform=val_test_transform)
+    
     # Dataloaders
-    num_workers = int(os.cpu_count()/2)
-    train_dataloader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=num_workers, drop_last=True, pin_memory=True,)
-    val_dataloader = DataLoader(dataset=val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=num_workers, drop_last=True, pin_memory=True,)
-    test_dataloader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=num_workers, drop_last=True, pin_memory=True,)
+    train_dataloader = DataLoader(
+        dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True,
+        num_workers=num_workers, drop_last=True, pin_memory=True,)
+    val_dataloader = DataLoader(
+        dataset=val_dataset, batch_size=BATCH_SIZE, shuffle=False,
+        num_workers=num_workers, drop_last=True, pin_memory=True,)
+    test_dataloader = DataLoader(
+        dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False,
+        num_workers=num_workers, drop_last=True, pin_memory=True,)
+    
     return train_dataloader, val_dataloader, test_dataloader
 
 class CIFAR10DataModule(LightningDataModule):
