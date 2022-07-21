71801b9326518ec3f0cbe2c171bd1f07955ce2a8
SihanA <57210715+Sihan-A@users.noreply.github.com>
2022-07-21 14:27:09 +0900

    label class

--- dataset/CIFAR10.py
+++ dataset/CIFAR10.py
@@ -5,6 +5,18 @@ import torch
 from pytorch_lightning import LightningDataModule
 import os
 
+cifar10_label_classes = {
+    0: "airplane",
+    1: "automobile",
+    2: "bird",
+    3: "cat",
+    4: "deer",
+    5: "dog",
+    6: "frog",
+    7: "horse",
+    8: "ship",
+    9: "truck",
+}
 cifar10_mean=[0.49139968, 0.48215841, 0.44653091]
 cifar10_std =[0.24703223, 0.24348513, 0.26158784]
 num_workers = int(os.cpu_count()/2)
