c33a0f9e9dea167ddd8e7da2cc845395ee898491
A <57210715+Sihan-A@users.noreply.github.com>
2022-08-07 23:21:13 +0900

    update

--- cv/cv_preprocess.py
+++ cv/cv_preprocess.py
@@ -2,11 +2,17 @@ import torch
 import numpy as np
     
 """
-- mean_std_dataloader
-- mean_std_dataset
+- mean_std_dataloader: calculate mean and standard deviation of a dataloader.
+- mean_std_dataset: calculate mean and standard deviation of a dataset.
 """
 
 def mean_std_dataloader(dataloader):
+    """ Calculate mean and standard deviation of a PyTorch dataloader.
+    Args:
+        dataloader: PyTorch DataLoader.
+    Returns:
+       mean, std: mean and standard deviation.
+    """
     channels_sum, channels_squared_sum, num_batches = 0, 0, 0
     for data, _ in dataloader:
         channels_sum += torch.mean(data, dim=[0,2,3])
@@ -17,6 +23,12 @@ def mean_std_dataloader(dataloader):
     return mean, std
 
 def mean_std_dataset(dataset):
+    """ Calculate mean and standard deviation of a PyTorch dataset.
+    Args:
+        dataloader: PyTorch Dataset.
+    Returns:
+       mean, std: mean and standard deviation.
+    """
     meanRGB = [np.mean(x.numpy(), axis=(1,2)) for x,_ in dataset]
     meanR = np.mean([m[0] for m in meanRGB])
     meanG = np.mean([m[1] for m in meanRGB])
