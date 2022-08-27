f7ca17ff2aae9f02c43fe73d06c9d8d702b42a8a
SihanA <57210715+Sihan-A@users.noreply.github.com>
2022-08-28 00:56:56 +0900

    colab

---
+++ colab.py
@@ -0,0 +1,14 @@
+from google.colab import files
+import os
+
+def kaggle_json_upload():
+    """ To upload the kaggle.json to use the kaggle dataset in colab
+    Usage:
+        from takhi.colab import kaggle_json_upload
+        kaggle_json_upload()
+    """
+    os.chdir("/content")
+    files.upload()
+    os.mkdir("/root/.kaggle")
+    os.rename("/content/kaggle.json", "/root/.kaggle/kaggle.json")
+    os.chmod("/root/.kaggle/kaggle.json", 600)
