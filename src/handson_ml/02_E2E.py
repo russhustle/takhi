cc5d746337168c9a200f7ed117d78e5474dc4bd9
SihanA <57210715+Sihan-A@users.noreply.github.com>
2022-09-03 20:34:15 +0900

    format

--- src/handson_ml/02_E2E.py
+++ src/handson_ml/02_E2E.py
@@ -4,14 +4,18 @@ import urllib.request
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
-    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+
+def save_fig(
+    image_path, fig_id, tight_layout=True, fig_extension="png", resolution=300
+):
+    path = os.path.join(image_path, fig_id + "." + fig_extension)
     print("Saving figure", fig_id)
     if tight_layout:
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
 
-def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+
+def fetch_housing_data(housing_url, housing_path):
     if not os.path.isdir(housing_path):
         os.makedirs(housing_path)
     tgz_path = os.path.join(housing_path, "housing.tgz")
@@ -20,6 +24,7 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
 
-def load_housing_data(housing_path=HOUSING_PATH):
+
+def load_housing_data(housing_path):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
