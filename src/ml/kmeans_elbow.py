d9f0562b9843cf8f0df25ee8d6f1ab62a4ab6305
A <57210715+Sihan-A@users.noreply.github.com>
2022-09-25 02:00:16 +0900

    Update and rename kmeans.py to kmeans_elbow.py

--- src/ml/kmeans_elbow.py
+++ src/ml/kmeans_elbow.py
@@ -3,16 +3,29 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from yellowbrick.cluster import KElbowVisualizer
 
+"""
+Plot the kmeans elbow method to find the best n components
+1. Use the below fucntion kmeans_elbow_plot
+2. Yellowbrick
+"""
+
 
 def kmeans_elbow_plot(data, num=(1, 10)):
-    # https://predictivehacks.com/k-means-elbow-method-code-for-python/
+    """
+    https://predictivehacks.com/k-means-elbow-method-code-for-python/
+    """
+    # initialization
     np.random.seed(42)
     distortions = []
     K = range(num[0], num[1])
+    
+    # model training
     for k in K:
         kmeanModel = KMeans(n_clusters=k)
         kmeanModel.fit(data)
         distortions.append(kmeanModel.inertia_)
+    
+    # plot
     plt.figure(figsize=(8, 4))
     plt.plot(K, distortions, "bx-")
     plt.xlabel("k")
